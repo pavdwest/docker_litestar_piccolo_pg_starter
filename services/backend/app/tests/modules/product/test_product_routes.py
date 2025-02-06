b683b62eec4c55b32b4f396c0e7d89d3c66b777f
pavdwest <pavdwesthuizen@gmail.com>
2025-02-06 23:08:35 +0100

    Try fix gh actions

--- services/backend/app/tests/modules/product/test_product_routes.py
+++ services/backend/app/tests/modules/product/test_product_routes.py
@@ -55,6 +55,7 @@ async def test_create_one_raises_error_for_duplicate(client: AsyncClient):
         "status_code": 409,
         "detail": 'duplicate key value violates unique constraint "product_title_key"\nDETAIL:  Key (title)=(test_create_one_raises_error_for_duplicate_title) already exists.',
     }
+    assert 1 == 2
 
 
 async def test_read_one(client: AsyncClient):
